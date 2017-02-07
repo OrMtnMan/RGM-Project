90600026df675c59b384722dcbf55f2ab3785c0a
W1Ndextor <straylinej@chc.edu>
2017-02-07 00:19:23 -0500

    created ORGM4570.txt, and wrote in Marlin1895GBL code. Still need to implement in loadmanager, and set up ammo table.

--- ORGM/media/lua/client/ORGMTest.lua
+++ ORGM/media/lua/client/ORGMTest.lua
@@ -5,9 +5,11 @@ function ORGMTest(_keyPressed)
     if key == 207 then
 		local player = getPlayer();
 		local testinv = player:getInventory();
-		testinv:AddItems("ORGM.9mmRounds", 15);
-		testinv:AddItem("ORGM.Ber92");
-		testinv:AddItem("ORGM.BerMag15");
+		testinv:AddItems("ORGM.4570Rounds", 15);
+		testinv:AddItem("ORGM.Marlin1895GBL");
+		testinv:AddItem("ORGM.4570HPRounds", 15);
+		testinv:AddItem("ORGM.4570HPBox");
+		testinv:AddItem(ORGM.4570Box");
 	end
 end
 
